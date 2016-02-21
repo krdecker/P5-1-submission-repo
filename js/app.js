80d5a5d89b053084886d55ff66908fba21b278ae
krdecker <krd.345@gmail.com>
2016-02-21 14:05:47 -0800

    clean-up

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 
-///////////////////MODEL///////////////////////
+////////////////////////////////////MODEL/////////////////////////////////////
 
 // eats data: an array of places to eat
 // around Commercial-Broadway Station
 // East Van
 // British Columbia
 
+// basic data, loaded if more detailed model not found in localStorage
 var EatsModel = {
     zoomLevel: 18,
     center: {lat: 49.262387, lng: -123.069768},//49.262387, -123.069768
@@ -45,28 +46,16 @@ var model = {};
 
 function getModel() {
     if (localStorage.model) {
-        console.log("found model in localStorage: ")
         return JSON.parse(localStorage.model);
     } else {
         return EatsModel;
     };
 };
 
-
-
-
-console.log(model);
-console.log(Date.now());
-
-
-
 function buildWindowContent(marker) {
-
     var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=200x150&location=' +
                             marker.position.toString() + '';
     var picture = '<img class="bgimg" src="' + streetviewUrl + '">';
-    //console.log("In buildContent: " + marker.position.toString());
-    //var css = '"height:100%;width:100%;font-size:4em;color:blue;background-color:orange;padding:5px"';
     var css = '"height:100%;width:80%"';
     var content = '<div onclick="itchwindow()" style=' +
                     css + '>' + picture + '<br><span style="color:darkgreen">' +
@@ -78,51 +67,12 @@ function buildWindowContent(marker) {
 
 //==========================================================
 // 3rd party API stuff
-
-// loc formatter function
-// re-format location coordinates
-// function getLocationAsLLString(name) {
-//     var result = "ll=";
-
-//     for (var i in model.spots) {
-//         var spot = model.spots[i];
-//         if (spot.name == name) {
-//             result += spot.location.lat.toString() + ',' + spot.location.lng.toString();
-//         }
-//     }
-
-//     return result;
-// }
-
-
-//from https://developer.foursquare.com/overview/auth#userless
-//https://api.foursquare.com/v2/venues/search?ll=40.7,-74&client_id=CLIENT_ID&client_secret=CLIENT_SECRET&v=YYYYMMDD
-
-//foursquare.com/v/broadway-station-sushi/4aae9cb7f964a5209b6220e3
-var kr = {
-    foursquare: {
-        Owner: "KR Decker",
-        Client_id: "ZVLKTKSGULFEQ3XMWWI3AL1A2KXYBEKD1LKSURUZDGZY41JX",
-        Client_secret: "TMT203DHWOPR3R4RSM1HE4RMOKEWLGTT0FLMZMUZ1OJHMAK1"
-    },
-    yelp: {
-        Consumer_Key:    "XrxJOUgGNKq9ZuWhBX1LDw",
-        Consumer_Secret: "zr2I76nT8l0O-trkupYE00B9BTA",
-        Token:   "-_F8aOxonR6q5SqvURwwgLIdU-qteS-B",
-        Token_Secret:    "GF7uRcPWCpzlHcRP3DiJWjvcXJE"
-    }
-};
-
 var fourSquareURL = 'https://api.foursquare.com';
-
 
 function buildSlideContent(spot) {
     var formattedData = "";
-    //var loc = getLocationAsLLString(spot.name);
     var loc = "ll=" + spot.location.lat.toString() + "," + spot.location.lng.toString();
     var ajax_error;
-    console.log(loc);
-
     // get the foursquare ID number of the spot
     $.ajax({
             url: fourSquareURL + '/v2/venues/search',
@@ -138,8 +88,6 @@ function buildSlideContent(spot) {
     }).success(function(response){
           getVenueInfo(response);
         });
-
-
     function getVenueInfo(data) {
         var venue = data.response.venues[0];
         //https://api.foursquare.com/v2/venues/VENUE_ID
@@ -158,73 +106,39 @@ function buildSlideContent(spot) {
     }).success(function(response){
           formatShowData(response);
         });
-
     }
-
     function formatShowData(data){
-        //console.log(data);
         var venue = data.response.venue;
-
-        //console.log(venue.name);
-            formattedData = 'FourSquare info: ' +
-                                '<br>' + '<br>' + venue.name + '<br>' +
-                                venue.contact.formattedPhone + '<br>' +
-                                '- ' + venue.location.address + '<br>' +
-                                '- ' + venue.location.city  + '<br>' +
-                                '- ' + venue.location.country  + '<br>' +
-                                'x-street: ' + venue.location.crossStreet + '<br>' +
-                                'Check-ins: ' + venue.stats.checkinsCount.toString() +
-                                '<br>' + venue.likes.summary + '<br>' + '<br>' +
-                                '<span style="background-color:#' + venue.ratingColor +
-                                ';color:black;padding:1%">' +
-                                'Rating: ' + venue.rating.toString() +
-                                '</span>' + '<br>' +
-                                '<img src="' + venue.bestPhoto.prefix +
-                                'cap300' + venue.bestPhoto.suffix + '">'
-                                ;
-        //console.log(formattedData);
+        formattedData = 'FourSquare info: ' +
+                        '<br>' + '<br>' + venue.name + '<br>' +
+                        venue.contact.formattedPhone + '<br>' +
+                        '- ' + venue.location.address + '<br>' +
+                        '- ' + venue.location.city  + '<br>' +
+                        '- ' + venue.location.country  + '<br>' +
+                        'x-street: ' + venue.location.crossStreet + '<br>' +
+                        'Check-ins: ' + venue.stats.checkinsCount.toString() +
+                        '<br>' + venue.likes.summary + '<br>' + '<br>' +
+                        '<span style="background-color:#' + venue.ratingColor +
+                        ';color:black;padding:1%">' +
+                        'Rating: ' + venue.rating.toString() +
+                        '</span>' + '<br>' +
+                        '<img src="' + venue.bestPhoto.prefix +
+                        'cap300' + venue.bestPhoto.suffix + '">'
+                        ;
         spot.slideContent = formattedData;
         console.log(spot.slideContent);
-        //vm.slideContent(formattedData);
-        //vm.slideOn(true);
     }
-
     function sendError(object, error, exception) {
         ajax_error = "Web call failed: " + error ;
-        console.log(ajax_error);
-        //vm.slideContent(ajax_error);
-        //vm.slideOn(true);
+        spot.slideContent = ajax_error;
     }
-
-    //return formattedData;
 }
 
-//TODO : build these results into the model and save in Local storage
+/////////////////////////////////////MAP VIEWMODEL////////////////////////////////////
 
-///////////////MAP VIEWMODEL///////////////////////////
-
-var map;//, markers;
+var map;
 var eatingSpots = [];
 var infowindow;
-
-
-//get rid of all Google's POI and Transit features on our map
-// var cleanSweep = [
-//         {
-//             featureType: "poi",
-//             elementType: "labels",
-//             stylers: [
-//                   { visibility: "off" }
-//             ]
-//         },
-//         {
-//             featureType: "transit",
-//             elementType: "labels",
-//             stylers: [
-//                   { visibility: "off"}
-//             ]
-//         }
-//       ];
 
 var RedRoadsNGreenBusStops = [
   {
@@ -268,47 +182,33 @@ var mapOptions = {
         styles: RedRoadsNGreenBusStops
     };
 
-
 function NoMap() {
     alert("Google Map is unavailable just now.\n" +
         "(you may want to check your internet connection) \n");
 }
 
 // invoked by map <script> in index.html
-function initMap() {
-
+function init() {
     map = new google.maps.Map(document.getElementById('map-div'), mapOptions );
-    model = getModel();
-    console.log("in initMap: " + Date.now());
-
-
-
+    model = getModel(); // load data object either from localStorage or file
+    // then build the slide content with ajax api calls, if necessary
     if (!(model.spots[0].slideContent)) {
         model.spots.forEach( function (spot) {
             buildSlideContent(spot);
         });
     };
-console.log(model);
     localStorage.model = JSON.stringify(model);
-    vm.spotList(model.spots);
-
+    viewModel.spotList(model.spots);
     infowindow = new google.maps.InfoWindow({});
-
     map.setZoom(model.zoomLevel);
     map.setCenter(model.center);
-
-    buildSpots(model.spots);
-
+    model.spots.forEach( function(spot) {
+        eatingSpots.push(new Spot(spot));
+    });
     // stay centred
     google.maps.event.addDomListener(window, 'resize', function() {
         map.setCenter(model.center);
     });
-}
-
-function buildSpots(spots) {
-    for (var i in spots) {
-        eatingSpots.push(new Spot(spots[i]));
-    }
 }
 
 // constructor
@@ -331,7 +231,6 @@ var Spot = function(data) {
 
 Spot.prototype.doOpening = function () {
     var that = this;
-
     cleanUpScreen();
     this.marker.setAnimation(google.maps.Animation.BOUNCE);
     window.setTimeout( function() {
@@ -340,98 +239,18 @@ Spot.prototype.doOpening = function () {
     }, 1000);
 };
 
-
-
-//  comment from Udacity Grader:
-// Your solution works, but it would be more object oriented to create a constructor function for a location like this:
-
-// var Location = function(data) {
-//   this.name = data.name;
-//   this.location = data.location;
-//   this.marker = new google.maps.Marker(/* ... */);
-// };
-// And populate a locations array instead of a separate markers array:
-
-// locations.push(new Location(spot));
-// You could also attach any function that modifies a single location as a method to this class:
-
-// Location.prototype.stopBounce = function() {
-//   this.marker.setAnimation(null);
-// };
-
 Spot.prototype.openWindow = function () {
-    //var content;
-    //console.log("In method openWindow: " + this.marker.title);
-    //console.log("Window content: " + this.windowContent);
-    //cleanUpScreen();
-    vm.listCollapse();
-
+    viewModel.listCollapse();
     infowindow.setContent(this.windowContent);
     infowindow.open(map, this.marker);
 }
 
-
-
-function itchwindow() {
-    vm.openAPIslide(infowindow.anchor.title);
-}
-
-
-// interface map/markers viewmodel to ko text filtration system
 Spot.prototype.hideMarker = function () { this.marker.setMap(null); };
 Spot.prototype.showMarker = function () { this.marker.setMap(map); };
 
 
-function getSpot(name) {
-    var result;
-    eatingSpots.forEach( function(spot) {
-       if (spot.name == name) result = spot;
-       //console.log("in getSpot: " + name);
-    });
-    return result;
-}
 
-function resetMarkers(spots) {
-    eatingSpots.forEach( function(spot) {
-        spot.hideMarker();
-    });
-
-    for (var i in spots) {
-        console.log("in function resetMarkers: " + spots[i].name);
-        eatingSpots.forEach( function(spot) {
-            if (spots[i].name == spot.name) spot.showMarker();
-        });
-    }
-
-}
-//     // var names = [];
-
-//     // for (var i in spots) names.push(spots[i].name);
-
-//     // for (var j in markers) {
-//     //     marker = markers[j];
-//     if (show) marker.setMap(map);
-//     else marker.setMap(null);
-//     // }
-// };
-
-function cleanUpScreen() {
-    infowindow.close();
-    map.setZoom(model.zoomLevel);
-    map.setCenter(model.center);
-    vm.slideOff();
-}
-
-function dubSlideContents() {
-    for (var i in model.spots) {
-        eatingSpots[i].slideContent = model.spots[i].slideContent;
-    }
-}
-
-/////////////////KO VIEWMODEL///////////////////
-
-
-
+////////////////////////////////////KO VIEWMODEL//////////////////////////////////
 
 var ViewModel = function () {
     var self = this;
@@ -440,45 +259,27 @@ var ViewModel = function () {
     self.listOn = ko.observable(true);
     self.burgerMenu = ko.observable(false);
     self.spotList = ko.observableArray();
-    console.log("in vm: " + self.spotList());
-
     self.slideOn = ko.observable(false);
     self.slideContent = ko.observable();
-
     self.filterSlot = ko.observable();
     self.isSelected = ko.observable(false);
 
-
     // Behaviours
-
     self.spotPick = function () {
-
-        // for (var i in eatingSpots) {
-        //     var spot = eatingSpots[i];
-        //     if (this.name == spot.name) spot.doOpening();
-        // }
-
         var spot = getSpot(this.name);
         spot.doOpening();
     };
-
     self.setFilterSelected = function() {
         this.isSelected(true);
         cleanUpScreen();
         self.listExpand();
     };
-
     self.filterSlot.subscribe(function(data) {
-
         self.listExpand();
-
         self.spotList(filterList(data, eatingSpots));
-
         resetMarkers(self.spotList());
     });
-
     self.onEnter = function (data, event) {
-
         if (event.keyCode === 13) {
             if (infowindow.anchor) {
                   self.openAPIslide(infowindow.anchor.title);
@@ -489,7 +290,6 @@ var ViewModel = function () {
                 }
             }
         }
-
         else {
             if (infowindow) infowindow.close(); //for any other keypress
             self.slideOff();
@@ -497,57 +297,33 @@ var ViewModel = function () {
         }
         return true; // necessary to reflect the text in the slot
     };
-
     self.listCollapse = function () {
         self.listOn(false);
         self.burgerMenu(true);
     };
-
     self.listExpand = function () {
         cleanUpScreen();
         self.burgerMenu(false);
         self.listOn(true);
     };
-
-//interface to API AJAX system
-
     self.openAPIslide = function (spotName) {
-
-        // builds content and sets it in slide
-        //var ajax_data =
-        //buildSlideContent(spotName);
         var spot = getSpot(spotName);
-        console.log(spot);
-         //spot.slideContent = formattedData;
         if (!(spot.slideContent)) {
             dubSlideContents();
         };
-        vm.slideContent(spot.slideContent);
-
-        vm.slideOn(true);
-
-        //console.log("In openAPI: ");//, ajax_data);
-        //self.slideContent(buildSlideContent(spotName));
-    //     if self.slideContent(ajax_data);
-
-    //     window.setTimeout(function () {
-    //         self.slideOn(true); //  will either display foursquare info or error message
-    //     }, 0);
+        viewModel.slideContent(spot.slideContent);
+        viewModel.slideOn(true);
     };
-
     self.slideOff = function () {
         this.slideOn(false);
     };
-
 };
 
-var vm = new ViewModel();
-ko.applyBindings(vm);
+var viewModel = new ViewModel();
+ko.applyBindings(viewModel);
 
-
-//-----------------------------------------------
-// helper funcs:
-
+//---------------------------------HELPER FUNCTIONS------------------------------
+//
 //test array against a regular expression
 function filterList(userText, modelArray) {
      var result=[],
@@ -559,7 +335,38 @@ function filterList(userText, modelArray) {
 
      return result;
 }
-
+function getSpot(name) {
+    var result;
+    eatingSpots.forEach( function(spot) {
+       if (spot.name == name) result = spot;
+    });
+    return result;
+}
+function resetMarkers(spots) {
+    eatingSpots.forEach( function(spot) {
+        spot.hideMarker();
+    });
+    for (var i in spots) {
+        getSpot(spots[i].name).showMarker();
+    }
+}
+function itchwindow() {
+    viewModel.openAPIslide(infowindow.anchor.title);
+}
+function cleanUpScreen() {
+    infowindow.close();
+    map.setZoom(model.zoomLevel);
+    map.setCenter(model.center);
+    viewModel.slideOff();
+}
+function dubSlideContents() {
+    if (model.spots[0].slideContent) {
+        for (var i in model.spots) {
+            eatingSpots[i].slideContent = model.spots[i].slideContent;
+            localStorage.model = JSON.stringify(model);
+        }
+    }
+}
 
 
 
@@ -611,4 +418,52 @@ function filterList(userText, modelArray) {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//from https://developer.foursquare.com/overview/auth#userless
+//https://api.foursquare.com/v2/venues/search?ll=40.7,-74&client_id=CLIENT_ID&client_secret=CLIENT_SECRET&v=YYYYMMDD
+
+//foursquare.com/v/broadway-station-sushi/4aae9cb7f964a5209b6220e3
+var kr = {
+    foursquare: {
+        Owner: "KR Decker",
+        Client_id: "ZVLKTKSGULFEQ3XMWWI3AL1A2KXYBEKD1LKSURUZDGZY41JX",
+        Client_secret: "TMT203DHWOPR3R4RSM1HE4RMOKEWLGTT0FLMZMUZ1OJHMAK1"
+    },
+    yelp: {
+        Consumer_Key:    "XrxJOUgGNKq9ZuWhBX1LDw",
+        Consumer_Secret: "zr2I76nT8l0O-trkupYE00B9BTA",
+        Token:   "-_F8aOxonR6q5SqvURwwgLIdU-qteS-B",
+        Token_Secret:    "GF7uRcPWCpzlHcRP3DiJWjvcXJE"
+    }
+};
 
